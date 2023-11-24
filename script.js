34326c6f5c81f0af8890c54a2e5bb64cdb4bb0d6
Stongwill <mishabasha1997@gmail.com>
2023-11-24 17:58:01 +0300

    finish project, comleted burger and fixed bags

--- script.js
+++ script.js
@@ -1,51 +1,49 @@
 
 
-const burger = document.querySelector('.header__burger');
+const burger = document.querySelectorAll('.header__burger');
 const header = document.querySelector('.header');
 const menu = document.querySelector('.header__menu');
 const headerBody = document.querySelector('.header__body');
 const headerLinks = document.querySelector('.header__links');
-const initialLogo = document.querySelector('.header__logo > img');
+const initialLogo = document.querySelector('.header__logo');
 
+const menuBurgerLinks = document.querySelectorAll('.modal-burger .header__item');
+
+const modalBurger = document.querySelector('.modal-burger')
 const modalBusiness = document.querySelector('.modal-business');
 
-const btnClose = document.querySelector('.modal-business__btn-closed')
+const btnClose = document.querySelectorAll('.modal-business__btn-closed')
 const btnOpenModal = document.querySelector('.profile__btn-tarife');
 
 const toggleBurger = (el) => {
     if(el){
-        el.addEventListener('click', (e) => {
-            // document.querySelector('.header__contacts-mob').classList.toggle('active');
-            // document.body.classList.toggle('noscroll');
-            // e.target.classList.toggle('active')
-            // document.querySelector('header').style.backgroundColor = '#fff';
-            // menu.classList.toggle('active');
-            // headerBody.classList.toggle('active')
+        el.forEach(burger => burger.addEventListener('click', (e) => {
+            document.body.classList.toggle('noscroll');
+            modalBurger.classList.toggle('active');
         })
-    }
+    )}
 
 }
-
 
 const adaptiveScroll = () => {
     const heightHeader = header.offsetHeight;
     const userScroll = window.scrollY;
     if(userScroll + heightHeader >= window.innerHeight - window.innerHeight){
         header.classList.add('active');
-        initialLogo.style.display = 'none';
+        initialLogo.classList.add('active');
     }
      if(userScroll === 0){
         header.classList.remove('active');
-        initialLogo.style.display = 'block';
+        initialLogo.classList.remove('active');
     }
 }
 
 
-const closeModal = (btn) => {
-    btn && btn.addEventListener('click', () => {
+const closeModal = (btn, modal) => {
+    btn && btn.forEach(button => button.addEventListener('click', () => {
         document.body.classList.remove('noscroll');
-        modalBusiness.classList.remove('active');
-    })
+        modal.classList.remove('active');
+    }))
 }
 
 const openModal = (btn) => {
@@ -53,17 +51,17 @@ const openModal = (btn) => {
         document.body.classList.add('noscroll');
         modalBusiness.classList.add('active');
 })
-    }
+}
 
 
-
-document.addEventListener('DOMContentLoaded', (e) => {
+document.addEventListener('DOMContentLoaded', () => {
     toggleBurger(burger);
     openModal(btnOpenModal);
-    closeModal(btnClose);
-
+    closeModal(btnClose, modalBusiness);
+    closeModal(menuBurgerLinks, modalBurger);
 })
 
 document.addEventListener('scroll', () => {
     adaptiveScroll()
 })
+
